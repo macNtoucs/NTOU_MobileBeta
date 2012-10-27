8d8d5f037a4f8055d51446ea8f2bd96ace03c1bf
macNtoucs <mac.ntoucs@gmail.com>
2012-10-27 16:20:06 +0800

    add definePixel.h

---
+++ Modules/Stellar/DefinePixel.h
@@ -0,0 +1,25 @@
+//
+//  DefinePixel.h
+//  MIT Mobile
+//
+//  Created by R MAC on 12/10/27.
+//
+//
+
+#ifndef MIT_Mobile_DefinePixel_h
+#define MIT_Mobile_DefinePixel_h
+
+#define UpperViewWidth 60
+#define UpperViewHeight UpperleftViewHeight
+
+#define UpperleftViewWidth 30
+#define UpperleftViewHeight 20
+
+#define LeftViewWidth UpperleftViewWidth
+#define LeftViewHeight 42
+
+#define NavigationAndStatusHeight 64
+
+#define TextLabelborderWidth 2
+
+#endif
