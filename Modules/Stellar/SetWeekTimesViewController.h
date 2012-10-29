edfadbbb5696c531402938505ca4a0de45edbf02
macNtoucs <mac.ntoucs@gmail.com>
2012-10-29 18:54:28 +0800

    add SetWeekTimesViewController

---
+++ Modules/Stellar/SetWeekTimesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetWeekTimesViewController.h
+//  MIT Mobile
+//
+//  Created by R MAC on 12/10/29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetWeekTimesViewController : UITableViewController
+
+@end
