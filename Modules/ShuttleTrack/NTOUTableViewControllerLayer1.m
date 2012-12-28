71372905fe5e4d58a2f9cbb433aa2d37e0a78848
macNtoucs <mac.ntoucs@gmail.com>
2012-12-28 12:07:12 +0800

    layer1加入高鐵

--- Modules/ShuttleTrack/NTOUTableViewControllerLayer1.m
+++ Modules/ShuttleTrack/NTOUTableViewControllerLayer1.m
@@ -186,11 +186,11 @@
         case 2:
             switch (indexPath.row) {
                 case 0:
-                    cell.textLabel.text= @"台鐵、國光";
+                    cell.textLabel.text= @"台鐵、高鐵、國光";
                     break;
             }
         default:
-            break;
+            break; 
     }
 
     return cell;
